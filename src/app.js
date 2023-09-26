3b94df39316876808b3b3b502364a068d513a136
Muhammad Awais <awaisshahid965@gmail.com>
2023-09-26 23:42:00 +0500

    socket io setup completely

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const path = require('path');
 
-const PORT = process.env.PORT || 3000
 const app = express()
 
 app.use(express.static(path.join(__dirname, 'public')))
@@ -13,10 +12,6 @@ app.set('views', path.join(__dirname, 'screens'));
 app.get('/', function(req, res) {
     res.render('index');
 });
-
-app.listen(PORT, () => {
-    console.log(`App is listening on port ${PORT}`)
-})
 
 module.exports = {
     app
